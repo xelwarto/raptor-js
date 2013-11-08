c3803d16e4ee43cdf229918dad1d2864d35d3f39
Ted <ted@domo.(none)>
2013-11-08 08:32:08 -0600

    Added pre-built version of the Raptor.JS

---
+++ Build/Raptor.JS/js/inc/ver.js
@@ -0,0 +1,5 @@
+
+var version = "v3.0";
+exports.getVersion = function() {
+	return version;
+};
---
+++ Build/Raptor.JS/js/raptor.js
@@ -0,0 +1,4 @@
+require("inc/title.js");
+var ver = require("inc/ver.js");
+
+out.println(ver.getVersion());
